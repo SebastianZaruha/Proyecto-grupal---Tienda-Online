8457fcb4a70ef6cd49b455d97af8529540616307
Zhi Li <zhi.s373721@cesurformacion.com>
2024-02-12 20:34:58 +0100

    footer basico falta informacion , decorar y retocar

---
+++ footer.php
@@ -0,0 +1,79 @@
+<head>
+    <style>
+        .container-fluid {
+            margin-top: 20vh;
+        }
+
+        a {
+            text-decoration: none;
+            color: black;
+        }
+
+        .col {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: start;
+        }
+
+        #redes .row2{
+            display: flex;
+            flex-direction: row;
+        }
+
+        #redes a {
+            color: #000;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container-fluid" style="width:100vw">
+        <div class="row" style="height: 20vh; background-color:#D2CBC4; ">
+            <div class="col"></div>
+            <div class="col">
+
+                <h4>registrate</h4>
+                <a href="#">lo que sea</a>
+                <a href="#">lo que sea</a>
+                <a href="#">lo que sea</a>
+            </div>
+            <div class="col">
+                <h4>acercate </h4>
+                <a href="#">lo que sea</a>
+                <a href="#">lo que sea</a>
+                <a href="#">lo que sea</a>
+            </div>
+            <div class="col">
+                <h4>Nuestras oliticas</h4>
+                <a href="#">lo que sea</a>
+                <a href="#">lo que sea</a>
+                <a href="#">lo que sea</a>
+            </div>
+            <div class="col">
+
+            </div>
+            <div class="col" id="redes">
+                <div class="row">
+                    <h4>redes</h4>
+                </div>
+
+                <div class="row2">
+                    <a href="#">
+                        <img src="./assets/redes/twitter.svg" alt="" height="30px" width="30px">
+                    </a>
+                    <a href="#">
+                        <img src="./assets/redes/reddit.svg" alt="" height="30px" width="30px">
+                    </a>
+                    <a href="#">
+                        <img src="./assets/redes/youtube.svg" alt="" height="30px" width="30px">
+                    </a>
+                    <a href="#">
+                        <img src="./assets/redes/instagram.svg" alt="" height="30px" width="30px">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
