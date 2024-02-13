50faafc2bf2a4cdf08d38b0f178c93c0a86995e7
Sergio Garcia <sergio.s374125@cesurformacion.com>
2024-02-13 14:11:46 +0100

    Navbar modificado. Añadido el menu desplegable con las categorias por separado.

--- navbar.php
+++ navbar.php
@@ -14,10 +14,26 @@
     <link href="https://fonts.googleapis.com/css2?family=Metrophobic&display=swap" rel="stylesheet">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
     <style>
         body {
             font-family: 'Metrophobic', sans-serif;
+        }
+
+        a {
+            text-decoration: none;
+            color: #000;
+        }
+
+        ul {
+            padding: 0;
+            margin: 0;
+        }
+
+        li {
+            list-style: none;
         }
 
         .navbar {
@@ -92,6 +108,7 @@
                 align-items: start !important;
             }
 
+            /* Ojo esto */
             .modal-content {
                 height: 50vh;
                 width: 80vw !important;
@@ -113,6 +130,41 @@
             transition: all 0.3s;
             background-color: rgba(0, 0, 0, 0.5);
         }
+
+
+        .mega-menu {
+            position: absolute;
+            top: 98%;
+            left: 0;
+            width: 100%;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            opacity: 0;
+            visibility: hidden;
+            transition: all 0.3s ease-in-out;
+        }
+
+
+        li:hover .mega-menu {
+            opacity: 1;
+            visibility: visible;
+            transition: all 0.1s ease-in-out;
+        }
+
+        .mega-menu a:hover {
+            font-weight: 600;
+        }
+
+        .mega-menu h5 {
+            font-size: 1.2rem;
+            font-weight: 600;
+            margin-bottom: .5rem;
+        }
+
+        .mega-menu li {
+            margin-bottom: .3rem;
+        }
     </style>
 
 </head>
@@ -120,7 +172,6 @@
 <body>
     <nav class="navbar navbar-expand-lg fixed-top py-0">
         <div class="container-fluid">
-
             <a class="search navbar-brand mx-4" href="#">
                 <img src="./assets/logo-navbar.png" alt="Logo" width="170" height="75">
             </a>
@@ -169,12 +220,111 @@
                     <ul class="navbar-nav  flex-grow-1" id="offCbody1">
                         <li class="nav-item me-5">
                             <a class="nav-link mx-lg-2" href="#">HOMBRE</a>
+                            <div class="mega-menu">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <h5>ROPA</h5>
+                                            <ul>
+                                                <li><a href="#">Camisas y camisetas</a></li>
+                                                <li><a href="#">Pantalones</a></li>
+                                                <li><a href="#">Shorts</a></li>
+                                                <li><a href="#">Chaquetas y abrigos</a></li>
+                                                <li><a href="#">Sudaderas</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>ZAPATOS</h5>
+                                            <ul>
+                                                <li><a href="#">Zapatillas</a></li>
+                                                <li><a href="#">Deportivas</a></li>
+                                                <li><a href="#">Formales</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>ACCESORIOS</h5>
+                                            <ul>
+                                                <li><a href="#">Relojes</a></li>
+                                                <li><a href="#">Gorras</a></li>
+                                                <li><a href="#">Gafas</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="nav-item me-5">
                             <a class="nav-link mx-lg-2" href="#">MUJER</a>
+                            <div class="mega-menu">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <h5>ROPA</h5>
+                                            <ul>
+                                                <li><a href="#">Camisas y blusas</a></li>
+                                                <li><a href="#">Pantalones</a></li>
+                                                <li><a href="#">Faldas</a></li>
+                                                <li><a href="#">Chaquetas y abrigos</a></li>
+                                                <li><a href="#">Vestidos y monos</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>ZAPATOS</h5>
+                                            <ul>
+                                                <li><a href="#">Zapatillas</a></li>
+                                                <li><a href="#">Botas</a></li>
+                                                <li><a href="#">Zapatos planos</a></li>
+                                                <li><a href="#">Zapatos de tacón</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>ACCESORIOS</h5>
+                                            <ul>
+                                                <li><a href="#">Bolsos</a></li>
+                                                <li><a href="#">Bisutería</a></li>
+                                                <li><a href="#">Sombreros</a></li>
+                                                <li><a href="#">Gafas</a></li>
+                                                <li><a href="#">Accesorios de pelo</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="nav-item me-5">
                             <a class="nav-link mx-lg-2" href="#">JOVEN</a>
+                            <div class="mega-menu">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <h5>ROPA</h5>
+                                            <ul>
+                                                <li><a href="#">Camisas y camisetas</a></li>
+                                                <li><a href="#">Pantalones</a></li>
+                                                <li><a href="#">Shorts</a></li>
+                                                <li><a href="#">Chaquetas y abrigos</a></li>
+                                                <li><a href="#">Sudaderas</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>ZAPATOS</h5>
+                                            <ul>
+                                                <li><a href="#">Zapatillas</a></li>
+                                                <li><a href="#">Deportivas</a></li>
+                                                <li><a href="#">Formales</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>ACCESORIOS</h5>
+                                            <ul>
+                                                <li><a href="#">Relojes</a></li>
+                                                <li><a href="#">Gorras</a></li>
+                                                <li><a href="#">Gafas</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -205,34 +355,36 @@
         <div style="display: none;" id="myModal">
             <div class="modal d-flex justify-content-center align-items-center ">
                 <div class="modal-dialog">
-                    <div class="modal-content " style="height: 50vh; width: 30vw; border: 1px solid black"">
-                    <div class=" modal-header">
-                        <h5 class="modal-title " id="miModalLabel">INICIA SESIÓN</h5>
-                        <button type="button" class="btn-close" onclick="closeModal()"></button>
-                    </div>
-                    <div class="modal-body m-3">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label
-                                ">Correo electrónico</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-
-                                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                                <div class="d-flex justify-content-center mt-3">
-                                    <button class="acceder btn mt-2 fs-5 text-white" style="background-color: #847C7C; width:30vw; height: 5vh;" type="submit">ACCEDER</button>
-                                </div>
-                            </div>
-                        </form>
-                        <hr class=" w-100" style="margin-top: 10%;">
-                        <div class=""><a href="" class="registrarse btn fs-5 text-white" style="background-color: #847C7C; width:100%; height: 5vh;">REGISTRASE </a>
+                    <div class="modal-content" style="height: 50vh; width: 30vw; border: 1px solid black">
+                        <div class=" modal-header">
+                            <h5 class="modal-title " id="miModalLabel">INICIA SESIÓN</h5>
+                            <button type="button" class="btn-close" onclick="closeModal()"></button>
                         </div>
+                        <div class="modal-body m-3">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Correo electrónico</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp">
 
+                                    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1">
+                                    <div class="d-flex justify-content-center mt-3">
+                                        <button class="acceder btn mt-2 fs-5 text-white"
+                                            style="background-color: #847C7C; width:30vw; height: 5vh;"
+                                            type="submit">ACCEDER</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <hr class=" w-100" style="margin-top: 10%;">
+                            <div class=""><a href="" class="registrarse btn fs-5 text-white"
+                                    style="background-color: #847C7C; width:100%; height: 5vh;">REGISTRARSE </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </nav>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
@@ -253,12 +405,12 @@
         });
 
         function openModal() {
-            var modal = document.getElementById("myModal");
+            let modal = document.getElementById("myModal");
             modal.style.display = "block";
         }
 
         function closeModal() {
-            var modal = document.getElementById("myModal");
+            let modal = document.getElementById("myModal");
             modal.style.display = "none";
         }
     </script>
