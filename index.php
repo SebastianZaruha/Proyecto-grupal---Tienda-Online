20d5b1226ac78b5cd46bb38835eceaae97164095
Sergio Garcia <sergio.s374125@cesurformacion.com>
2024-02-13 16:25:16 +0100

    Commit pre-merge con footer

--- index.php
+++ index.php
@@ -54,22 +54,8 @@ require_once 'navbar.php';
     <div class="container">
         <div class="row">
             <h3>PRODUCTOS POPULARES</h3>
-        </div>
-        <div class="row">
-            <div id="carouselExample" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="./assets/images/slider1-inicio/slider1.jpg" class="d-block w-100" alt="Imagen 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="./assets/images/slider1-inicio/slider2.jpg" class="d-block w-100" alt="Imagen 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="./assets/images/slider1-inicio/slider3.jpg" class="d-block w-100" alt="Imagen 3">
-                    </div>
 
-                </div>
-            </div>
+
         </div>
     </div>
 
