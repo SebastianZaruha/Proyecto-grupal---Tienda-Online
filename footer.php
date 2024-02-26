b7dfaee55a020fc7b76d5cbbceba8ef886622d2b
daniel <daniel.s238681@cesurformacion.com>
2024-02-26 16:32:58 +0100

    footer terminado

--- footer.php
+++ footer.php
@@ -8,6 +8,14 @@
             color: #000;
             padding: 3%;
         }
+
+        .footer-col ul li:hover a {
+            color: #808080;
+        }
+
+        .footer-col ul li a:hover img {
+            opacity: 0.5;
+        }
     </style>
 
 </head>
@@ -16,39 +24,39 @@
     <footer class=" p-4 text-center" style="margin-top: 15%;">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">MRTK</h5>
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0 footer-col">
+                    <h5 class="text-uppercase">Velour</h5>
                     <p>
-                        Info o una frase guapa. Algo de texto simempre viene bien.
+                        Viste tu estilo, define tu elegancia: Velour.
                     </p>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0 footer-col">
                     <h5 class="text-uppercase">Sobre nosotros</h5>
                     <ul class="list-unstyled mb-0">
-                        <li><a href="#!">Enlace 1</a></li>
-                        <li><a href="#!">Enlace 2</a></li>
-                        <li><a href="#!">Enlace 3</a></li>
-                        <li><a href="#!">Enlace 4</a></li>
+                        <li><a href="#!">Sobre Velour</a></li>
+                        <li><a href="#!">Sostenibilidad</a></li>
+                        <li><a href="#!">Nuestras tiendas</a></li>
+                        <li><a href="#!">Devoluciones</a></li>
                     </ul>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0 footer-col">
                     <h5 class="text-uppercase">Contacto</h5>
                     <ul class="list-unstyled mb-0">
-                        <li><a href="#!">Email</a></li>
-                        <li><a href="#!">Teléfono</a></li>
+                        <li><a href="#!">velour@gmail.com</a></li>
+                        <li><a href="#!">+34 630548914</a></li>
                         <li><a href="#!">Dirección</a></li>
                     </ul>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0 footer-col">
                     <h5 class="text-uppercase">Nuestras redes</h5>
                     <ul class="list-unstyled mb-0 d-flex justify-content-center">
-                        <li class="me-2"><a href="#!"><img src="./assets/redes/facebook.svg" height="30px" width="30px" alt="redes1"></a></li>
-                        <li class="me-2"><a href="#!"><img src="./assets/redes/instagram.svg" height="30px" width="30px" alt="redes2"></a></li>
-                        <li class="me-2"><a href="#!"><img src="./assets/redes/twitter.svg" height="30px" width="30px" alt="redes3"></a></li>
-                        <li class="me-2"><a href="#!"><img src="./assets/redes/youtube.svg" height="30px" width="30px" alt="redes4"></a></li>
+                        <li class="me-2"><a href="https://www.facebook.com"><img src="./assets/redes/facebook.svg" height="30px" width="30px" alt="redes1"></a></li>
+                        <li class="me-2"><a href="https://www.instagram.com"><img src="./assets/redes/instagram.svg" height="30px" width="30px" alt="redes2"></a></li>
+                        <li class="me-2"><a href="https://www.twitter.com"><img src="./assets/redes/twitter.svg" height="30px" width="30px" alt="redes3"></a></li>
+                        <li class="me-2"><a href="https://www.youtube.com"><img src="./assets/redes/youtube.svg" height="30px" width="30px" alt="redes4"></a></li>
                     </ul>
                 </div>
             </div>
