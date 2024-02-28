634b23e1a7e8ca535611c849346e0a6b5dab111e
Sergio Garcia <sergio.s374125@cesurformacion.com>
2024-02-28 13:57:08 +0100

    Arreglado el responsive de imprescindibles. Falta hacer los botones funcionales.

--- imprescindibles.php
+++ imprescindibles.php
@@ -1,10 +1,25 @@
-<div class="row justify-content-center" style="height: 70vh;">
-    <div class="col-12 col-sm-12 col-md-8 col-lg-5" style="background-image: url(./assets/images/pava.avif);">
-        <br><br>
-        <button type="button" class="btn btn-light ms-4"><strong>COMPRAR AHORA</strong></button>
+<style>
+    .image-container {
+        height: 70vh;
+        position: relative;
+    }
+
+    .image-container button {
+        position: absolute;
+        top: 30px;
+        left: 40px;
+    }
+</style>
+
+<div class="row justify-content-center">
+    <div class="col-sm-12 col-md-6 col-lg-5 px-1 image-container">
+        <img src="./assets/images/pava.avif" alt="Imagen 1"
+            style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+        <button type="button" class="btn btn-light"><strong>COMPRAR AHORA</strong></button>
     </div>
-    <div class="col-12 col-sm-12 col-md-8 col-lg-5" style="background-image: url(./assets/images/png2.avif);">
-        <br><br>
-        <button type="button" class="btn btn-light ms-4"><strong>COMPRAR AHORA</strong></button>
+    <div class="col-sm-12 col-md-6 col-lg-5 px-1 image-container">
+        <img src="./assets/images/png2.avif" alt="Imagen 2"
+            style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+        <button type="button" class="btn btn-light"><strong>COMPRAR AHORA</strong></button>
     </div>
 </div>
